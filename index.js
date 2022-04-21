3cad330d30afd773bd797189ae279ef833520576
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-21 19:56:20 +0200

    Added gsap to change the color of the background

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-import gsap from 'gsap'
+import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const infos = [
   {
-    'image': new URL('static/strawberry1.jpg', import.meta.url),
-    'title': 'Strawberries',
+    'image': new URL('static/fruits.jpg', import.meta.url),
+    'title': 'Fruits',
     'detail': ['Model: Ajsa Njie', 'MU: Sabrina Wolf', 'Bern, Switzerland'],
+    'color': '#fff1d8'
   },
   {
-    'image': new URL('static/strawberry2.jpg', import.meta.url),
-    'title': 'Strawberries',
+    'image': new URL('static/power.jpg', import.meta.url),
+    'title': 'Power',
     'detail': ['Model: Ajsa Njie', 'MU: Sabrina Wolf', 'Bern, Switzerland'],
+    'color': '#d8ffdb'
   },
   {
-    'image': new URL('static/pineapple1.jpg', import.meta.url),
-    'title': 'Pineapple',
+    'image': new URL('static/minimal.jpg', import.meta.url),
+    'title': 'Minimal',
     'detail': ['Model: Ajsa Njie', 'MU: Sabrina Wolf', 'Bern, Switzerland'],
+    'color': '#f0f0f0'
   },
   {
-    'image': new URL('static/pineapple2.jpg', import.meta.url),
-    'title': 'Pineapple',
+    'image': new URL('static/neon.jpg', import.meta.url),
+    'title': 'Neon',
     'detail': ['Model: Ajsa Njie', 'MU: Sabrina Wolf', 'Bern, Switzerland'],
+    'color': '#d8fff6'
+  },
+  {
+    'image': new URL('static/light.jpg', import.meta.url),
+    'title': 'Light',
+    'detail': ['Model: Ajsa Njie', 'MU: Sabrina Wolf', 'Bern, Switzerland'],
+    'color': '#fff1d8'
   }
 ]
 
-const insertDiv = (array, container) => {
+const insertSection = (array, container) => {
   array.forEach((e, index) => {
-    let div = document.createElement('div')
-    div.classList.add('box')
-    container.appendChild(div)
-    insertImage(array, div, index)
+    let section = document.createElement('section')
+    section.classList.add('box')
+    section.setAttribute('data-color', array[index]['color']);
+    container.appendChild(section)
+    insertImage(array, section, index)
   })
 }
 
@@ -65,9 +76,37 @@ const insertDetail = (array, container, index) => {
   container.appendChild(detailContainer)
 }
 
-const fadeIn = (image) => {
-
-}
-
 let container = document.querySelector('.container')
-insertDiv(infos, container)
+insertSection(infos, container)
+
+
+// Change color
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.utils.toArray(".box").map((elem) => {
+
+  var color = elem.getAttribute('data-color');
+
+  let trigger = ScrollTrigger.create({
+      trigger: elem,
+      start: 'top 5%',
+      end: 'bottom 5%',
+      markers: true,
+      onToggle() {
+        gsap.to("body", {
+          backgroundColor: color,
+          duration: 1.4
+        });
+      }
+  });
+
+  return () => {
+    color = elem.getAttribute('data-color');
+    if (trigger.isActive) {
+      gsap.killTweensOf("body");
+      gsap.set("body", {
+        backgroundColor: color
+      })
+    }
+  }
+});
